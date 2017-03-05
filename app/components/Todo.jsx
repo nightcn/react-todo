6812348ccbb5567a87179398cb287c7ff14dcfc1
tsahibspele <tsahibspele@gmail.com>
2017-03-05 21:34:03 +0200

    Add timestamp support

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,16 +1,24 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
 	handleOnClick: function () {
 		this.props.onToggle(this.props.id);
 	},
 	render: function () {
-		var {id, text, completed} = this.props;
+		var {id, text, completed, createdAt, completedAt} = this.props;
+		var renderDate = () => {
+			var message = completed ? 'Completed ' : 'Created ';
+			var timestamp = completed ? completedAt : createdAt;
+
+			return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+		}
 
 		return (
 			<div onClick={this.handleOnClick}>
 				<input type="checkbox" checked={completed} />
-				{text}
+				<p>{text}</p>
+				<p>{renderDate()}</p>
 			</div>
 		);
 	}
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -21,13 +21,16 @@ describe('TodoApp', () => {
 		todoApp.handleAddTodo(todoText);
 
 		expect(todoApp.state.todos[0].text).toBe(todoText);
+		expect(todoApp.state.todos[0].createdAt).toBeA('number');
 	});
 
 	it('should toggle completed value when handleToggle called', () => {
 		var todoData = {
 			id: 11,
 			text: 'test features',
-			completed: false
+			completed: false,
+			createdAt: 34,
+			completedAt: undefined
 		};
 
 		var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
@@ -37,6 +40,30 @@ describe('TodoApp', () => {
 
 		todoApp.handleToggle(11);
 		expect(todoApp.state.todos[0].completed).toBe(true);
+		expect(todoApp.state.todos[0].completedAt).toBeA('number');
+
+	});
+
+	it('should set completedAt value to undefined when completed is false', () => {
+		var todoData = {
+			id: 11,
+			text: 'test features',
+			completed: false,
+			createdAt: 34,
+			completedAt: undefined
+		};
+
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+		todoApp.setState({todos: [todoData]});
+
+		expect(todoApp.state.todos[0].completed).toBe(false);
+
+		todoApp.handleToggle(11);
+		expect(todoApp.state.todos[0].completed).toBe(true);
+		expect(todoApp.state.todos[0].completedAt).toBeA('number');
+
+		todoApp.handleToggle(11);
+		expect(todoApp.state.todos[0].completedAt).toBe(undefined);
 
 	});
 });
