d20a69630202a9c48a59873c19076170526d29c3
tsahibspele <tsahibspele@gmail.com>
2017-03-17 10:12:40 +0200

    Refactor TodoList, Todo, and AddTodo

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -3,28 +3,36 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var TestUtils = require('react-addons-test-utils');
 
-var AddTodo = require('AddTodo');
+var {AddTodo} = require('AddTodo');
 
 describe('AddTodo', () => {
 	it('should exist', () => {
 		expect(AddTodo).toExist();
 	});
 
-	it('should call onAddTodo if valid input entered', () => {
+	it('should dispatch ADD_TODO when valid input entered', () => {
+		
+		var todoText = 'Test todo';
+		var action = {
+			type: 'ADD_TODO',
+			text: todoText
+		};
+
 		var spy = expect.createSpy();
-		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy} />);
 		var $el = $(ReactDOM.findDOMNode(addTodo));
-		var todoText = 'test'
+		
+
 
 		addTodo.refs.todoText.value = todoText;
 		TestUtils.Simulate.submit($el.find('form')[0]);
 
-		expect(spy).toHaveBeenCalledWith(todoText);
+		expect(spy).toHaveBeenCalledWith(action);
 	});
 
-	it('should not call onAddTodo if invalid input entered', () => {
+	it('should not dispatch addTodo when invalid input entered', () => {
 		var spy = expect.createSpy();
-		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy} />);
 		var $el = $(ReactDOM.findDOMNode(addTodo));
 		var todoText = ''
 
